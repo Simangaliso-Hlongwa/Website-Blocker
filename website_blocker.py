11d668827e9899a0e2b02c8fc7a8982ee994bf5d
Simangaliso <74491200+Simangaliso-Hlongwa@users.noreply.github.com>
2025-02-24 00:52:56 +0200

    Update website_blocker.py
    added comments

--- website_blocker.py
+++ website_blocker.py
@@ -1,16 +1,15 @@
 import time
-from datetime import datetime as dt #assigning the module to dt to keep the script short
+from datetime import datetime as dt
 
 host_temp = "hosts"
-host_path = r"C:\Windows\System32\drivers\etc" #r tells python that the backslashes are not new lines
-redirect = "127.0.0.1" #this is the redirect of the sites you find distracting
-website_list = ["www.wikipedia.org", "wikipedia.org", "www.facebook.com", "facebook.com", "www.instagram.com","instagram.com", "www.x.com", "x.com"]
+host_path = r"C:\Windows\System32\drivers\etc" 
+redirect = "127.0.0.1" 
+website_list = ["www.facebook.com", "facebook.com", "www.instagram.com","instagram.com", "www.x.com", "x.com"]
 
 """
 I use a while to keep the script running and adding the blocked websites
 the while loops executes the actions very fast
 specifically this loop runs every 5 seconds
-dt.(dt.now(year)) returns the current year
 """
 while True:
     if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,14): #here we compare the datetime objects with the current time
@@ -22,7 +21,10 @@ while True:
                     pass
                 else:
                     file.write(redirect + " " + website + "\n")
-
+"""
+rewriting each line of the file until we find the line containing the website
+then we delete everything below the newly written file
+"""
     else:
         with open(host_path, 'r+') as file:
             content = file.readlines()
@@ -30,6 +32,6 @@ while True:
             for line in content:
                 if not any(website in line for website in website_list):
                     file.write(line)
-                file.truncate()  #this line deletes everything downwards or from the last line downwards
+                file.truncate()  
         print("Break Hours...")
-    time.sleep(5)  #scrpit is now run every 5 seconds, but the delay will be 5 seconds after the cuttoff
+    time.sleep(5)
